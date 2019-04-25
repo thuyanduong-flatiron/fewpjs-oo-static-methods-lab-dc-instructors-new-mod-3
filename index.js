2fc480a2b1bf64cbf0214e1ae270ab299edd8f99
thuyanduong-flatiron <thuyanannduong@gmail.com>
2019-04-25 15:29:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return `${string[0].toLocaleUpperCase()}${string.slice(1)}`
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9- ']+/g, '');
+  }
+  static titleize(string){
+    let wordArray = string.split(' ')
+    let titleArray = []
+    titleArray.push(Formatter.capitalize(wordArray[0]))
+    for(let i = 1; i<wordArray.length; i++){
+      if(wordArray[i] === 'the' || wordArray[i] === 'a' || wordArray[i] === 'an' || wordArray[i] === 'but' || wordArray[i] === 'of' || wordArray[i] === 'and' || wordArray[i] === 'for' || wordArray[i] === 'at' || wordArray[i] === 'by' || wordArray[i] === 'from'){
+        titleArray.push(wordArray[i])
+      }else{
+        titleArray.push(Formatter.capitalize(wordArray[i]))
+      }
+    }
+    return titleArray.join(' ')
+  }
 }
